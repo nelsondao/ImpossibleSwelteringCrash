38524b005b4d8e1756f8170e0347521e67758062
ndao <Nelson.Dao@patelhs.org>
2020-11-21 13:11:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+   int[] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+   String[] names = new String[15]; 
+
+   Scanner scan = new Scanner(System.in);
+  
+  for (int i = 0; i < 15; i++){
+  System.out.println("What is your names?");
+   //Put a name in, so you can get assigned a seat
+  names[i] = scan.next(); 
+  //This scanner will look for 15 names to assign the user. So each name the user puts in gets assigned a seat
+  }
+  int count = 0;
+  while(count <= 14) {
+    
+  }
+   }
+  }
+
+
+  
